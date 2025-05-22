21ca17c475f4e2d2d0f8b23a13caa3d9a84e27b1
wangjiayue <2060923313@qq.com>
2025-05-22 15:23:10 +0800

    2
    豆宝ai界面添加了js和wxss，home界面有改动

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,17 +1,22 @@
 Page({
-  data: {},
-  onLoad() {
-    // 可加入未来数据请求，如推荐路线动态获取
-  },
-  goToGuide() {
-    wx.navigateTo({
-      url: '/pages/scene/index'
-    });
-  },
-  goToGoods() {
-    wx.switchTab({
-      url: '/pages/goods/goods'
-    });
-  }
+    data: {},
+    onLoad() {
+      // 可预留未来扩展
+    },
+    goToGuide() {
+      wx.navigateTo({
+        url: '/pages/scene/index'
+      });
+    },
+    goToGoods() {
+      wx.switchTab({
+        url: '/pages/goods/goods'
+      });
+    },
+    dou() {
+      wx.navigateTo({
+        url: '/pages/dou/index' 
+      });
+    }
+  });
   
-});
